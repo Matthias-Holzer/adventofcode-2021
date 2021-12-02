adb44b681e94cffa7a91eecde732c88216486966
Matthias-Holzer <wien.matthias.holzer@gmail.com>
2021-12-02 19:12:09 +0100

    für carlos

---
+++ day1/day1.py
@@ -0,0 +1,26 @@
+file = open('input', 'r')
+read = file.read().splitlines()
+numbers = [int(i) for i in read]
+
+def one():
+    counter = 0
+    for idx,number in enumerate(numbers):
+        if idx != 0:
+            if number > numbers[idx-1]:
+                counter += 1
+    print(counter)
+
+def two():
+    counter = 0
+    for idx, number in enumerate(numbers):
+        if idx != 0:
+            try:
+                if number + numbers[idx - 1] + numbers[idx-2] > numbers[idx - 1] + numbers[idx-2] + numbers[idx-3]:
+                    counter += 1
+            except:
+                if numbers > numbers[idx-1]:
+                    counter += 1
+
+    print(counter)
+
+two()
---
+++ day2/day2.py
@@ -0,0 +1,37 @@
+file = open('input', 'r')
+read = file.read().replace('\n',' ').split(" ")
+numbers = [int(i) for i in read[1::2]]
+data = read
+data[1::2]=numbers
+print(data)
+
+def one():
+    horizontal = 0
+    vertical = 0
+    for idx,d in enumerate(data[::2]):
+        if d == 'forward':
+            horizontal += data[idx*2+1]
+        if d == 'up':
+            vertical -= data[idx*2+1]
+        if d == 'down':
+            vertical += data[idx*2+1]
+
+    print(horizontal)
+    print(vertical)
+    print(horizontal*vertical)
+
+def two():
+    horizontal = 0
+    depth = 0
+    aim = 0
+    for idx,d in enumerate(data[::2]):
+        if d == 'forward':
+            horizontal += data[idx*2+1]
+            depth += aim * data[idx*2+1]
+        if d == 'up':
+            aim -= data[idx*2+1]
+        if d == 'down':
+            aim += data[idx*2+1]
+    print(depth*horizontal)
+
+two()
