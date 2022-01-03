5b02f08fda94252d734eb86d1349f1f1d886e21a
Matthias-Holzer <wien.matthias.holzer@gmail.com>
2022-01-03 15:48:53 +0100

    day 4 2

--- day4/day4.py
+++ day4/day4.py
@@ -4,6 +4,8 @@ tables = None
 tables_hit = []
 draws = {}
 last_draw = None
+part_two = False
+bords_that_won = []
 
 def writeToCSV(rows):
     fieldnames = ['table_id', 'idx', 'idy', 'value', 'ticked']
@@ -33,37 +35,38 @@ def one():
                     if item == draw:
                         tables_hit[idx_table][idx_row][idx_item] = 1
         check_for_bingo()
-        print(tables_hit)
-        #exit()
+
+def two():
+    global part_two
+    part_two = True
+    one()
 
 def check_for_bingo():
     for idx,table in enumerate(tables_hit):
         for row in table:
             if 0 not in row: #checks verticaly
                 print('FOUND ONE HORIZONTAL')
-                print(row)
                 calculate_winning_board(idx)
-                #exit()
         for num in range(5): #checks horizontaly
             vertical = [table[0][num],table[1][num],table[2][num],table[3][num],table[4][num]]
             if 0 not in vertical:
                 print('FOUND ONE VERTICAL')
-                print(vertical)
-                print(tables[idx])
                 calculate_winning_board(idx)
-                #exit()
         left_to_right = [table[0][0],table[1][1],table[2][2],table[3][3],table[4][4]]
         right_to_left = [table[0][4],table[1][3],table[2][2],table[3][1],table[4][0]]
         if 0 not in left_to_right:
             print('FOUND ONE DIAGONAL left_to_right')
             calculate_winning_board(idx)
-            #exit()
         if 0 not in right_to_left:
             print('FOUND ONE DIAGONAL right_to_left')
             calculate_winning_board(idx)
-            #exit()
 
 def calculate_winning_board(winning_board_number):
+    if not winning_board_number in bords_that_won:
+        bords_that_won.append(winning_board_number)
+    if part_two:
+        if len(tables) != len(bords_that_won):
+            return
     sum = 0
     umarkt_numbers = []
     for idx_row,row in enumerate(tables[winning_board_number]):
@@ -73,19 +76,9 @@ def calculate_winning_board(winning_board_number):
                 umarkt_numbers.append(item)
     print(f"The Winning Summ is: {sum*last_draw}")
     print(f"The Winning Board is {winning_board_number}")
-    print_table(tables[winning_board_number])
     print(f"The last Draw was {last_draw}")
     print(f"The sum is {sum} and the index is {winning_board_number}")
-    print(f"Ther umarkt numbers are {umarkt_numbers}")
-    print(draws)
-    print(tables)
-    print_table(tables)
     exit()
-
-def print_table(table):
-    for row in table:
-        print("TABLE X")
-        print(row)
 
 def main():
     global draws
@@ -116,10 +109,9 @@ def main():
             new5 = []
 
     tables = new4
-    print(tables)
-    print_table(tables)
-    print(tables_hit)
-    one()
+
+    #one()
+    two()
 
 if __name__ == "__main__":
     main()
